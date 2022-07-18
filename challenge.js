5a36eaf1b04b8eec4d732f3b0332a98149346540
Arbaz Khan <arbaazkhanark23@gmail.com>
2022-07-19 03:40:41 +0530

    Challege Completed

--- challenge.js
+++ challenge.js
@@ -12,24 +12,24 @@ Suppose we get data from a web service about a certain game (below). In this cha
 
 3.Create an array 'allPlayers' containing all players of both team (22 players)
 
-4.During the game, Arbaz (team 1) used 3 subsititute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+4.During the game, Arbaz (team 1) used 3 subsititute players. So create a new array ('players1Final') containing all the original team1 players plus 'Jamshed', 'Kulbir' and 'Naman'.
 
 5.Based on the game.odds object, create one variable for each odd(called 'team1','draw' and 'team2').
 
 
 6.Write a function ('printGoals') that receives an arbitrary number of player names (NOT an Array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
 
-7. The team with the lower odd is more likely to wiin. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
 
-TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scorerd.
+TEST DATA FOR 6: Use players 'Aman', 'Mukul', 'Jai' and 'Sonu'. Then, call the function again with players from game.scored.
 
 GOOD LUCK😄
 */
 
 
 const game={
-    team1:'Bayern Munich',
-    team2:'Borrussia Dortmund',
+    team1:'Arbaz',
+    team2:'Laxman',
     players:[
         [
             'Arbaz',
@@ -37,7 +37,7 @@ const game={
             'Ayush',
             'Kartik',
             'Deepak',
-            'Vikas',
+            'Lokesh',
             'Ashfaq',
             'Chayank',
             'Vickey',
@@ -57,7 +57,7 @@ const game={
         ],
     ],
     score:'4:0',
-    scored: ['Lewandowski','Gnarby','Lewandowskki','Hummels'],
+    scored: ['Chirag','Lokesh','Rahul','Arbaz'],
     date:'Nov 9th, 2037',
     odds:{
         team1:1.33,
@@ -71,22 +71,46 @@ const [players1,players2]=game.players;
 console.log(players1);
 console.log(players2);
 
+// 2.
 const [team1Goal, ...fieldPlayers1]=[...players1];
 const [team2Goal, ...fieldPlayers2]=[...players2];
 console.log('Goal Keeper: ',team1Goal);
 console.log('fieldPlayers1: ',fieldPlayers1);
 console.log('Goal Keeper: ',team2Goal);
 console.log('fieldPlayers2: ',fieldPlayers2);
-const [...allPlayers]=[players1,players2];
-console.log('All players: ',allPlayers);
+
 
 const [...gk]=[team1Goal, team2Goal];
 console.log("Goal Keepers: ", gk);
 
+// 3.
+const [...allPlayers]=[players1,players2];
+console.log('All players: ',allPlayers);
 
-
+// 4.
 const [...players1Final]=[...players1,'Jamshed','Kulbir','Naman'];
 console.log('players1Final: ',players1Final);
 
+// 5.
+const {odds:{team1,x:draw,team2}}=game;
+console.log(team1,draw,team2);
+
+//6.
+const printGoals=function(...players){
+    console.log(players);
+    console.log(`${players.length} goals were scored`)
+}
+
+printGoals(game.scored);
+// printGoals('Chirag','Lokesh','Rahul','Arbaz');
+// printGoals('Arbaz','Aman');
+
+
+// 7.
+team1<team2 && console.log('Team 1 is more likely to win');
+team1>team2 && console.log('Team 2 is more likely to win');
+
+
+/////////////////Challenge Completed ///////////////////////////////////////
 
 
